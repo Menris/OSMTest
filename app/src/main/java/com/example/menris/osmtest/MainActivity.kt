13e06b1b4c5bd9789a40e97ffe75fd52ee2e203e
amirdautov <amir_dautov95@mail.ru>
2018-08-08 19:16:12 +0600

    map corrections

--- app/src/main/java/com/example/menris/osmtest/MainActivity.kt
+++ app/src/main/java/com/example/menris/osmtest/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.menris.osmtest
 
 import android.Manifest
-import android.support.v7.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
 import permissions.dispatcher.NeedsPermission
 import permissions.dispatcher.RuntimePermissions
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory
-import org.osmdroid.views.MapView
-import android.preference.PreferenceManager
-import kotlinx.android.synthetic.main.activity_main.*
-import org.osmdroid.config.Configuration
 
 
 @RuntimePermissions
@@ -19,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     fun showLocation() {
     }
 
+    @SuppressLint("MissingPermission")
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Configuration.getInstance().load(applicationContext, PreferenceManager.getDefaultSharedPreferences(applicationContext))
@@ -26,6 +33,39 @@ class MainActivity : AppCompatActivity() {
 
         myMap.setTileSource(TileSourceFactory.MAPNIK)
         myMap.controller.setZoom(15.0)
+
+        val mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        mFusedLocationClient.lastLocation
+                .addOnSuccessListener(this) {
+                    it.latitude
+                    myMap.controller.animateTo(GeoPoint(it.latitude, it.longitude))
+                }
+                .addOnFailureListener(this) { }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu) //your file name
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.menu_main_get_data -> {
+                /*mFusedLocationClient.lastLocation
+                        .addOnSuccessListener(this) {
+                            it.latitude
+                            myMap.controller.animateTo(GeoPoint(it.latitude, it.longitude))
+                        }
+                        .addOnFailureListener(this) {
+                            toast("Местоположение не найдено")
+                        }*/
+                true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     public override fun onResume() {
